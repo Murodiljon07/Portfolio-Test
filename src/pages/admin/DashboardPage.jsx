e7960ad241a3d703ea4881bb3a6cfea0a619b335
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-03-09 19:10:43 +0500

    first commit

--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import { aboutReq } from "../../services/about.service";
+import { Outlet } from "react-router-dom";
 
 function DashboardPage() {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default DashboardPage;
