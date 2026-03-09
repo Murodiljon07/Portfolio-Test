da7463b28fbd574f0b76a653a51dfdf2ed9297a4
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-03-09 14:01:46 +0500

    first commit

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HeroSection from "../../components/public/HeroSection";
+import AboutMeSection from "../../components/public/AboutMeSection";
 
 function HomePage() {
   return (
     <div>
-      <section>
-        <HeroSection />
-      </section>
+      <HeroSection />
+      <AboutMeSection />
+      {/* add additional sections here as you continue implementing the Figma design */}
     </div>
   );
 }
